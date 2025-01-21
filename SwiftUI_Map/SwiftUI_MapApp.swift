79e5af5d3da851a4af81e48696ec70a3bf8ca47c
Romain Tirbisch <romain_tirbisch@yahoo.com>
2025-01-21 18:35:47 +0100

    Initial Commit

---
+++ SwiftUI_Map/SwiftUI_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapApp.swift
+//  SwiftUI_Map
+//
+//  Created by Elfo on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
